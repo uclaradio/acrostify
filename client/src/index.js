e01ea7e93f2679933698fc05c9649ffb2e033e65
OsbertReynaldi <83745982+OsbertReynaldi@users.noreply.github.com>
2022-11-18 12:09:44 -0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
